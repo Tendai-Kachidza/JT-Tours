c5fc9ca2a8e77634a5623789f5c0b5932d5f7d6d
Tendai <tendaikachidza5@gmail.com>
2025-08-15 10:01:57 +0200

    Initial commit

---
+++ js/allscript.js
@@ -0,0 +1,102 @@
+let navbar = document.querySelector('.header .navbar');
+
+document.querySelector('#menu-btn').onclick = () =>{
+    navbar.classList.add('active');
+}
+
+document.querySelector('#nav-close').onclick = () =>{
+    navbar.classList.remove('active');
+}
+
+let searchForm = document.querySelector('.search-form');
+
+document.querySelector('#search-btn').onclick = () =>{
+    searchForm.classList.add('active');
+}
+
+document.querySelector('#close-search').onclick = () =>{
+    searchForm.classList.remove('active');
+}
+
+window.onscroll = () =>{
+    navbar.classList.remove('active');
+
+    if(window.scrollY > 0){
+        document.querySelector('.header').classList.add('active');
+    }else{
+        document.querySelector('.header').classList.remove('active');
+    }
+};
+
+window.onload = () =>{
+    if(window.scrollY > 0){
+        document.querySelector('.header').classList.add('active');
+    }else{
+        document.querySelector('.header').classList.remove('active');
+    }
+};
+
+
+
+
+
+
+// -------------------------------------------to top scroller------------------------------------
+
+const toTop = document.querySelector(".to-top");
+
+window.addEventListener("scroll", () => {
+  if (window.pageYOffset > 100) {
+    toTop.classList.add("active");
+  } else {
+    toTop.classList.remove("active");
+  }
+})
+
+
+
+
+
+
+// --------------------------------------------product main page js----------------------------------
+
+let tabs = document.querySelectorAll('.tabs__toggle'),
+    contents = document.querySelectorAll('.tabs__content');
+
+tabs.forEach((tab, index) => {
+    tab.addEventListener('click', () => {
+        contents.forEach((content) => {
+            content.classList.remove('is-active');
+        });
+        tabs.forEach((tab) => {
+            tab.classList.remove('is-active');
+        });
+        contents[index].classList.add('is-active');
+        tabs[index].classList.add('is-active');
+    });
+});
+
+
+const track = document.querySelector('.carousel-track');
+const nextBtn = document.querySelector('.carousel-btn.next');
+const prevBtn = document.querySelector('.carousel-btn.prev');
+const slides = document.querySelectorAll('.carousel-track img');
+let currentIndex = 0;
+
+function updateCarousel() {
+    const slideWidth = slides[0].clientWidth;
+    track.style.transform = `translateX(-${currentIndex * slideWidth}px)`;
+}
+
+nextBtn.addEventListener('click', () => {
+    currentIndex = (currentIndex + 1) % slides.length;
+    updateCarousel();
+});
+
+prevBtn.addEventListener('click', () => {
+    currentIndex = (currentIndex - 1 + slides.length) % slides.length;
+    updateCarousel();
+});
+
+window.addEventListener('resize', updateCarousel);
+
